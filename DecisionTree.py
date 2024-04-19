9bb635c694de40244c5eddce15a2d2850b790007
Frankhuex <HU0021AN@e.ntu.edu.sg>
2024-04-19 14:54:11 +0800

    small changes

--- DecisionTree.py
+++ DecisionTree.py
@@ -8,14 +8,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import plot_tree
 class DecisionTree:
-    def __init__(self,data,x,y,depth=10):
+    def __init__(self,data,x,y,max_depth=10):
         self.x=x
         self.y=y
         self.x_df=pd.DataFrame(data[x])
         self.y_df=pd.DataFrame(data[y])
         self.x_train,self.x_test,self.y_train,self.y_test= train_test_split(self.x_df, self.y_df, test_size = 0.25)
         self.xy_train= pd.concat([self.x_train, self.y_train], axis = 1).reindex(self.x_train.index)
-        self.apply_tree(depth)
+        self.apply_tree(max_depth)
 
     def x_information(self):
         if type(self.x)!=str:
@@ -41,13 +41,28 @@ class DecisionTree:
             return
         f, axes = plt.subplots(1, 1, figsize=(18, 24))
         sb.violinplot(x = self.x, y =self.y, data =self.xy_train, orient = "h",order=order)
-    def apply_tree(self,depth):
-        self.tree = DecisionTreeClassifier(max_depth = depth)
+    def apply_tree(self,max_depth):
+        self.tree = DecisionTreeClassifier(max_depth = max_depth)
         self.tree.fit(self.x_train,self.y_train)
         self.y_train_pred=self.tree.predict(self.x_train)
         self.y_test_pred=self.tree.predict(self.x_test)
-    def apply_RandomForest(self,n_estimators=5,depth=10):        
-        self.tree=RandomForestClassifier(n_estimators=n_estimators,criterion='gini',max_depth=depth)
+    def apply_RandomForest(
+            self,
+            n_estimators=20,
+            max_depth=10,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            max_features=7,
+            criterion='gini'
+        ):        
+        self.tree=RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            max_features=max_features,
+            criterion=criterion,
+        )
         self.tree.fit(self.x_train,self.y_train)
         self.y_train_pred=self.tree.predict(self.x_train)
         self.y_test_pred=self.tree.predict(self.x_test)
